55ede371f546db3b8491aa621464fdedd7f07dfc
GroovinChip <groovinchip@gmail.com>
2020-02-25 14:48:31 -0500

    Web Annotations in sidebar (#1)
    * start experimenting with redux store
    
    * WA now appear in sidebar, remove deprecated popup code
    
    * comments

--- src/WebAnnotationsPlugin.js
+++ src/WebAnnotationsPlugin.js
@@ -1,62 +1,18 @@
 import React from 'react'
 import isEqual from 'lodash/isEqual'
 import { getVisibleCanvases } from 'mirador/dist/es/src/state/selectors/canvases'
+import { receiveAnnotation } from 'mirador/dist/es/src/state/actions/annotation'
 import { withStyles } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
-import Dialog from '@material-ui/core/Dialog'
 import MuiDialogTitle from '@material-ui/core/DialogTitle'
 import MuiDialogContent from '@material-ui/core/DialogContent'
 import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
 import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
-import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser'
-
-// Define styles for components
-const styles = theme => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2)
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500]
-  }
-})
-
-// This component represents the title of the Dialog
-// that will show Web Annotation data
-const DialogTitle = withStyles(styles)(props => {
-  const { children, classes, onClose, ...other } = props
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h2">{children}</Typography>
-      {onClose ? (
-        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  )
-})
-
-// This component represents the content of the Dialog
-// that will show Web Annotation data
-const DialogContent = withStyles(theme => ({
-  root: {
-    padding: theme.spacing(2)
-  }
-}))(MuiDialogContent)
-
-// --------------------------------------------------------------------------------------------------------------------
 
 // This component represents the entire custom Mirador plugin
 class WebAnnotationsPlugin extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { body: '', open: false };
     this.fetchAnnotations = this.fetchAnnotations.bind(this);
   }
 
@@ -64,6 +20,10 @@ class WebAnnotationsPlugin extends React.Component {
   // that points to our Web Annotation endpoint. If there are no 
   // annotations for a given canvas, we display a message stating so.
   fetchAnnotations(canvases) {
+    const { receiveAnnotation } = this.props;
+    if (!canvases) {
+      return
+    }
     canvases.forEach(canvas => {
       if (canvas) {
         var iiifUrl = canvas.id;
@@ -88,35 +48,21 @@ class WebAnnotationsPlugin extends React.Component {
         fetch(url, {
           method: 'GET',
         }).then(res => res.json())
-          .then((results) => {          
-            if (results.body != undefined) {
-              this.setState({
-                body: results.body[0].value
-              });
-            } else { // Handle case where results has no body      
-              this.setState({
-                body: 'No annotations available'
-              });
+          .then((results) => { 
+            const AnnoPage = {
+              id: url,
+              type: 'AnnotationPage',
+              items: [
+                results,
+              ],
             }
+            receiveAnnotation(canvas.id, url, AnnoPage) // results should be annotation page
         }, (error) => {
           console.log(error);
-          this.setState({
-            body: 'No annotations available'
-          });
         });
       }
     });
   }
-
-  // This function opens the Dialog
-  openDialog () {
-    this.setState({ open: true })
-  }
-
-  // This function closes the Dialog
-  closeDialog () {
-    this.setState({ open: false })
-  }  
 
   componentDidMount() {
     const { canvases } = this.props;
@@ -133,34 +79,32 @@ class WebAnnotationsPlugin extends React.Component {
     }
   }
 
-  // Turn the Web Annotation's transcription into viewable HTML
+  // 
   render() {
     return (
-      <div>
-        <Button onClick={this.openDialog.bind(this)} >View Web Annotations</Button>
-        <Dialog open={this.state.open} onClose={this.closeDialog.bind(this)}>
-        <DialogTitle id="customized-dialog-title" onClose={this.closeDialog.bind(this)}>Web Annotations</DialogTitle>
-          <DialogContent>
-            <Box>{ReactHtmlParser(this.state.body)}</Box>
-          </DialogContent>
-        </Dialog>
-      </div> 
+      <this.props.TargetComponent {...this.props.targetProps}></this.props.TargetComponent>
     )
   }
 } 
 
 // Hook into Mirador's state to get the canvases
-function mapStateToProps(state, { windowId }) {
+function mapStateToProps(state, props) {
   return {
-    canvases: getVisibleCanvases(state, { windowId: windowId }),
+    canvases: getVisibleCanvases(state, { windowId: props.targetProps.windowId }),
     config: state.config,
   };
 };
 
+// Add annotations to Mirador state
+const mapDispatchToProps = {
+  receiveAnnotation: receiveAnnotation
+}
+
 export default {
   name: "WebAnnotationsPlugin",
-  target: "WindowTopBarPluginMenu",
-  mode: "add",
+  target: "WindowCanvasNavigationControls",
+  mode: "wrap",
   component: WebAnnotationsPlugin,
   mapStateToProps: mapStateToProps,
+  mapDispatchToProps: mapDispatchToProps,
 };
